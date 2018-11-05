fe89d15298325638a86805ce66a5e50fe23e37e2
Kenny Peel <knowonehome@gmail.com>
2018-11-05 13:08:36 -0600

    #4

---
+++ src/main/java/com/knowonehome/tutorialmod/init/ModBlocks.java
@@ -0,0 +1,17 @@
+package com.knowonehome.tutorialmod.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.knowonehome.tutorialmod.blocks.BlockBase;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class ModBlocks 
+{
+	public static final List<Block> BLOCKS = new ArrayList<Block>();
+	
+	public static final Block RUBY_BLOCK = new BlockBase("ruby_block", Material.IRON);
+	
+}
